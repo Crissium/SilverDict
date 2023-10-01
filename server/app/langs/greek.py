674a37e5ea8012b9ade54de59db70e1fd7b131b8
Xing Yi <blandilyte@gmail.com>
2023-10-02 00:00:18 +0800

    feat: bidirectional Greek transliteration

--- server/app/langs/greek.py
+++ server/app/langs/greek.py
@@ -1,39 +1,54 @@
 """
 This module provides transliteration support for Greek, ancient and modern.
-The transliteration scheme is 'Beta Code', as recommended by Jiang Qian.
-It defines an injective mapping from Greek to Latin letters:
+The transliteration scheme is 'Beta Code', which defines a bijective mapping from Greek to Latin letters:
 α β γ δ ε ζ η θ ι κ λ μ ν ξ ο π ρ σ ς τ υ φ χ ψ ω
 a b g d e z h q i k l m n c o p r s s t u f x y w
 
-Note that it is only to be used to transliterate Greek into Latin, not the other way around.
-The idea is, sometimes a Greek lexicon's headwords are written in Latin, and we desire
-to query with Greek letters. This module provides a way to do that.
+(It is indeed bijective, ς is used only at the end of a word.)
 """
 
 import unicodedata
+import re
 
-TRANSLITERATION_TABLE = str.maketrans(
+_EL_TO_LA_TRANSLITERATION_TABLE = str.maketrans(
 	'αβγδεζηθικλμνξοπρσςτυφχψω',
 	'abgdezhqiklmncoprsstufxyw'
 )
 
+_LA_TO_EL_TRANSLITERATION_TABLE = str.maketrans(
+	'abgdezhqiklmncoprstufxyw',
+	'αβγδεζηθικλμνξοπρστυφχψω'
+)
+
+_FINAL_SIGMA_PATTERN = re.compile(r'σ\b|σ$')
+
+def _transliterate_latin_into_greek(s: 'str') -> 'str':
+	s = s.translate(_LA_TO_EL_TRANSLITERATION_TABLE)
+	# Replace all σ's that are at the end of a word with ς
+	s = _FINAL_SIGMA_PATTERN.sub('ς', s)
+	return s
+
 def is_greek(s: 'str') -> 'bool':
 	"""
-	Check if a string contains Greek letters.
+	Check if a string contains Greek or Latin letters.
 	"""
 	for c in s:
-		if unicodedata.name(c).startswith('GREEK'):
+		if unicodedata.name(c).startswith('GREEK') or unicodedata.name(c).startswith('LATIN'):
 			return True
 	return False
 
-def transliterate(greek: 'str') -> 'list[str]':
+def transliterate(s: 'str') -> 'list[str]':
 	"""
-	Transliterates Greek into Latin.
+	Bidirectional transliteration between Greek and Latin letters.
 	"""
-	return [greek, greek.translate(TRANSLITERATION_TABLE)]
+	return [_transliterate_latin_into_greek(s), s.translate(_EL_TO_LA_TRANSLITERATION_TABLE)]
 
 if __name__ == '__main__':
 	assert is_greek('αγαθος')
-	assert transliterate('αγαθος') == 'agaqos'
+	assert 'agaqos' in transliterate('αγαθος')
 	assert is_greek('ψυχη')
-	assert transliterate('ψυχη') == 'yuxh'
+	assert 'yuxh' in transliterate('ψυχη')
+	assert is_greek('agaqos')
+	assert 'αγαθος' in transliterate('agaqos')
+	assert is_greek('yuxh')
+	assert 'ψυχη' in transliterate('yuxh')
