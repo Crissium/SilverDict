42138834c7116ea4b5bac1a309def0cc89292af6
Yi Xing <blandilyte@gmail.com>
2024-02-13 20:47:47 +0800

    feat: new translations en-US and zh-CN

--- client/src/l10n.js
+++ client/src/l10n.js
@@ -1,9 +1,13 @@
 import LocalizedStrings from 'react-localization';
 import { RTL_LANGS } from './config';
 import enGB from './translations/en-GB.json';
+import enUS from './translations/en-US.json';
+import zhCN from './translations/zh-CN.json';
 
 export const localisedStrings = new LocalizedStrings({
 	'en-GB': enGB,
+	'en-US': enUS,
+	'zh-CN': zhCN
 });
 
 export const interfaceLangIsRTL = RTL_LANGS.includes(localisedStrings.getLanguage());
